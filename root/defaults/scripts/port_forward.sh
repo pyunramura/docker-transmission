760df069faeb85048c40596f0b7c8abf48279cf9
Pyun Ramura <pyun.ramura+gitlab@gmail.com>
2019-01-21 00:15:40 -0600

    port_forward not working with auth->set peer-port in settings manually

--- root/defaults/scripts/port_forward.sh
+++ root/defaults/scripts/port_forward.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env sh
 SERVICE=transmission-daemon
+SETTINGS=/config/settings.json
 PARAM=peer-port
 PORT=$(cat /var/run/openvpn/vpn_port)
-if [ ! "$PORT" = "" ]; then
-  /usr/bin/transmission-remote --port $PORT
-  echo "[services.d] [INFO] $SERVICE $PARAM was updated to $PORT"; else
-    echo "[services.d] [WARN] $SERVICE $PARAM is unavailable; check that the vpn tunnel is up"
-fi
+CONFIG=`cat $SETTINGS | jq '.["peer-port"]' | sed 's/"//g'`
+MODCFG=`cat $SETTINGS | sed "s/.*$PARAM.*/    \"$PARAM\"\: $PORT,/g" > temp.json`
+change_settings () {
+  s6-svc -wD -d /var/run/s6/services/transmission
+  $MODCFG
+  chown abc:abc temp.json
+  mv temp.json /config/settings.json
+  s6-svc -wu -u /var/run/s6/services/transmission
+  break ;
+}
+case "$PORT_F" in
+     yes) if [ ! "$PORT" = "$CONFIG" ]; then
+          echo "[system.d] [INFO] changing $SERVICE $PARAM to $PORT"
+          change_settings; else
+          if [ "$PORT" = "" ]; then
+          echo "[system.d] [WARN] PORT_F=yes, check that /var/run/openvpn/vpn_port is accessible"
+          fi
+          fi ;;
+       *) break ;;
+esac
 exit 0
