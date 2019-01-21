2c5a06e17626419d85d70107f176f88dce596b8d
Pyun Ramura <pyun.ramura+gitlab@gmail.com>
2019-01-20 20:55:43 -0600

    called scripts are not respecting sleep, moved sleep delay to run

--- root/defaults/scripts/port_forward.sh
+++ root/defaults/scripts/port_forward.sh
@@ -2,7 +2,6 @@
 SERVICE=transmission-daemon
 PARAM=peer-port
 PORT=$(cat /var/run/openvpn/vpn_port)
-sleep 20
 if [ ! "$PORT" = "" ]; then
   /usr/bin/transmission-remote --port $PORT
   echo "[services.d] [INFO] $SERVICE $PARAM was updated to $PORT"; else
